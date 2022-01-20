6f88b89ba9c2f7a654e8c3fe3b2729bd998eda03
RoninTest <testronintest@gmail.com>
2022-01-20 21:55:44 +0300

    First Commit

---
+++ W3Source/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace W3Source
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtNumber1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.lblNumber1 = new System.Windows.Forms.Label();
+            this.lblNumber2 = new System.Windows.Forms.Label();
+            this.txtNumber2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnHomework2 = new System.Windows.Forms.Button();
+            this.btnSingle = new System.Windows.Forms.Button();
+            this.btnHomework3 = new System.Windows.Forms.Button();
+            this.lblTopic = new System.Windows.Forms.Label();
+            this.lblHomeworks = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNumber1
+            // 
+            this.txtNumber1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumber1.Location = new System.Drawing.Point(207, 53);
+            this.txtNumber1.Name = "txtNumber1";
+            this.txtNumber1.Size = new System.Drawing.Size(110, 31);
+            this.txtNumber1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // lblNumber1
+            // 
+            this.lblNumber1.AutoSize = true;
+            this.lblNumber1.Font = new System.Drawing.Font("Yu Gothic", 15.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumber1.Location = new System.Drawing.Point(74, 56);
+            this.lblNumber1.Name = "lblNumber1";
+            this.lblNumber1.Size = new System.Drawing.Size(102, 27);
+            this.lblNumber1.TabIndex = 2;
+            this.lblNumber1.Text = "Number1";
+            // 
+            // lblNumber2
+            // 
+            this.lblNumber2.AutoSize = true;
+            this.lblNumber2.Font = new System.Drawing.Font("Yu Gothic", 15.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumber2.Location = new System.Drawing.Point(74, 107);
+            this.lblNumber2.Name = "lblNumber2";
+            this.lblNumber2.Size = new System.Drawing.Size(108, 27);
+            this.lblNumber2.TabIndex = 4;
+            this.lblNumber2.Text = "Number 2";
+            // 
+            // txtNumber2
+            // 
+            this.txtNumber2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumber2.Location = new System.Drawing.Point(207, 104);
+            this.txtNumber2.Name = "txtNumber2";
+            this.txtNumber2.Size = new System.Drawing.Size(110, 31);
+            this.txtNumber2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(79, 194);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 40);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Homework-1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnHomework2
+            // 
+            this.btnHomework2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHomework2.ForeColor = System.Drawing.Color.Red;
+            this.btnHomework2.Location = new System.Drawing.Point(79, 240);
+            this.btnHomework2.Name = "btnHomework2";
+            this.btnHomework2.Size = new System.Drawing.Size(106, 41);
+            this.btnHomework2.TabIndex = 6;
+            this.btnHomework2.Text = "Homework-2";
+            this.btnHomework2.UseVisualStyleBackColor = true;
+            this.btnHomework2.Click += new System.EventHandler(this.btnHomework2_Click);
+            // 
+            // btnSingle
+            // 
+            this.btnSingle.BackColor = System.Drawing.Color.DarkKhaki;
+            this.btnSingle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSingle.Location = new System.Drawing.Point(377, 66);
+            this.btnSingle.Name = "btnSingle";
+            this.btnSingle.Size = new System.Drawing.Size(109, 26);
+            this.btnSingle.TabIndex = 7;
+            this.btnSingle.Text = "Single Value";
+            this.btnSingle.UseVisualStyleBackColor = false;
+            this.btnSingle.Click += new System.EventHandler(this.btnSingle_Click);
+            // 
+            // btnHomework3
+            // 
+            this.btnHomework3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHomework3.ForeColor = System.Drawing.Color.Red;
+            this.btnHomework3.Location = new System.Drawing.Point(79, 287);
+            this.btnHomework3.Name = "btnHomework3";
+            this.btnHomework3.Size = new System.Drawing.Size(106, 40);
+            this.btnHomework3.TabIndex = 8;
+            this.btnHomework3.Text = "Homework-3";
+            this.btnHomework3.UseVisualStyleBackColor = true;
+            this.btnHomework3.Click += new System.EventHandler(this.btnHomework3_Click);
+            // 
+            // lblTopic
+            // 
+            this.lblTopic.AutoSize = true;
+            this.lblTopic.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.lblTopic.Font = new System.Drawing.Font("Yu Gothic", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTopic.Location = new System.Drawing.Point(266, 155);
+            this.lblTopic.Name = "lblTopic";
+            this.lblTopic.Size = new System.Drawing.Size(152, 19);
+            this.lblTopic.TabIndex = 9;
+            this.lblTopic.Text = "Homework Buttons";
+            // 
+            // lblHomeworks
+            // 
+            this.lblHomeworks.AutoSize = true;
+            this.lblHomeworks.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.lblHomeworks.Font = new System.Drawing.Font("Yu Gothic", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHomeworks.Location = new System.Drawing.Point(286, 9);
+            this.lblHomeworks.Name = "lblHomeworks";
+            this.lblHomeworks.Size = new System.Drawing.Size(132, 27);
+            this.lblHomeworks.TabIndex = 10;
+            this.lblHomeworks.Text = "Homeworks";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblHomeworks);
+            this.Controls.Add(this.lblTopic);
+            this.Controls.Add(this.btnHomework3);
+            this.Controls.Add(this.btnSingle);
+            this.Controls.Add(this.btnHomework2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblNumber2);
+            this.Controls.Add(this.txtNumber2);
+            this.Controls.Add(this.lblNumber1);
+            this.Controls.Add(this.txtNumber1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumber1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label lblNumber1;
+        private System.Windows.Forms.Label lblNumber2;
+        private System.Windows.Forms.TextBox txtNumber2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnHomework2;
+        private System.Windows.Forms.Button btnSingle;
+        private System.Windows.Forms.Button btnHomework3;
+        private System.Windows.Forms.Label lblTopic;
+        private System.Windows.Forms.Label lblHomeworks;
+    }
+}
+
---
+++ W3Source/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace W3Source
+{
+    public partial class Form1 : Form
+    {
+
+        int number1;
+        int number2;
+        double sonuc;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GetTxtNumber1()
+        {
+            number1 = Convert.ToInt32(txtNumber1.Text); 
+        }
+
+        private void GetTxtNumber2()
+        {
+            if (txtNumber2.Enabled != false)
+                number2 = Int32.Parse(txtNumber2.Text);
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            number1 = Convert.ToInt32(txtNumber1.Text);
+            number2 = Convert.ToInt32(txtNumber2.Text);
+
+            if (number1 == number2)
+            {
+                MessageBox.Show("Values are equal");
+            }
+            else
+            {
+                MessageBox.Show("Values aren't equal");
+            }
+        }
+
+        private void btnHomework2_Click(object sender, EventArgs e)
+        {
+            GetTxtNumber1();
+            GetTxtNumber2();
+
+            sonuc = number1 % 2;
+            if(sonuc == 0)
+            {
+                MessageBox.Show("The even value");
+            }
+            else
+            {
+                MessageBox.Show("The odd value");
+            }
+            
+
+        }
+
+        private void btnSingle_Click(object sender, EventArgs e)
+        {
+            txtNumber2.Enabled = false;
+            MessageBox.Show("Input must be single value");
+        }
+
+        private void btnHomework3_Click(object sender, EventArgs e)
+        {
+            
+
+            if (number1 > 0)
+            {
+                MessageBox.Show("The Value is positive.");
+            }else if (number1 < 0)
+            {
+                MessageBox.Show("The Value is negetive.");
+            }
+            else if(number1 == 0)
+            {
+                MessageBox.Show("'0' is neither a positive nor a negative value.  ");
+            }else
+            {
+                MessageBox.Show("Please, Enter a value ");
+            }
+        }
+    }
+}
